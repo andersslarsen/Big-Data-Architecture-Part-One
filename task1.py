09556df03724ccb10f69a85d828c4fee1e348bd0
thomasfosen <thomalf@stud.ntnu.no>
2021-03-02 11:53:01 +0100

    Added task1.py

---
+++ task1.py
@@ -0,0 +1,32 @@
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession
+import sys
+
+
+conf = SparkConf().setAppName("project").setMaster("local[*]")
+sc = SparkContext(conf = conf)
+spark = SparkSession(sc)
+sc.setLogLevel("ERROR")
+
+#Directory path from argument
+INPUT_DATA_PATH = sys.argv[1]
+
+
+#Load datasets to RDDs
+
+badgesRDD = sc.textFile(INPUT_DATA_PATH + "/badges.csv.gz")
+commentsRDD = sc.textFile(INPUT_DATA_PATH + "/comments.csv.gz")
+postsRDD = sc.textFile(INPUT_DATA_PATH + "/posts.csv.gz")
+usersRDD = sc.textFile(INPUT_DATA_PATH + "/users.csv.gz")
+
+
+badgesRows = badgesRDD.count()-1
+commentsRows = commentsRdd.count() - 1
+postsRows = postsRDD.count() - 1
+usersRows = usersRDD.count() - 1
+
+
+print("The number of columns in the badges RDD: " + str(badgesRows))
+print("The number of columns in the comments RDD: " + str(commentsRows))
+print("The number of columns in the posts RDD: " + str(postsRows))
+print("The number of columns in the users RDD: " + str(usersRows))
